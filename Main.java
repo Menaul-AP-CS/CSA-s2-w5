6762eedb60e87227d932a8557fb964699fc0bc55
Chidunbo <jliu2@olin.edu>
2026-02-09 13:29:31 -0700

    Add answer sheet

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // StandardAlgorithmsTestCases.testFindMin();
-        // StandardAlgorithmsTestCases.testFindAvg();
+        StandardAlgorithmTest.testFindMin();
+        // StandardAlgorithmTestCases.testFindAvg();
         // StandardAlgorithmsTestCases.testFindMode();
         // StandardAlgorithmsTestCases.testSearchOne();
         // StandardAlgorithmsTestCases.testAllNegative();
---
+++ StandardAlgorithms.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class StandardAlgorithms {
+    // Algorithm 1: Find Min (Same thing for Max)
+    public static int findMin(ArrayList<Integer> arr){
+       
+    }
+
+    // Algorithm 2: Find Average
+    public static double findAvg(ArrayList<Integer> arr){
+        
+    }
+
+    // Algorithm 3: mode of elements
+    public static int findMode(ArrayList<Integer> arr){
+       
+    }
+
+    // Algorithm 4: Search for a particular element in the array
+    // This method will return the index of the
+    // first element found in the array that is an even number
+    public static int searchOne(ArrayList<Integer> arr){
+       
+    }
+
+
+    // Algorithm 5: Determine if all elements have a particular property
+    // This method returns true if all elements are negative
+    public static boolean allNegative(ArrayList<Integer> arr){
+   
+    }
+
+
+    // Algorithm 6: Count all consecutive pairs of elements
+    // This method returns the number of consecutive pairs
+    // For example: [2,3,3,1,1,3,2,2] has three consecutive pairs
+    public static int countConsecutivePairs(ArrayList<Integer> arr){
+       
+    }
+
+
+    // Algorithm 7: Determine the presence or absence of duplicate elements
+    // This method returns ture if the if there are duplicated elements
+    // for example: [1,2,3,1] -> true, 1 is duplicated
+    public static boolean hasDuplicates(ArrayList<Integer> arr){
+       
+    }
+
+    // Algorithm 8: Determine the number of elements meeting specific criteria
+    // This method counts how many of the String-type elements in the ArrayList 
+    // starts with the letter "a"
+    // Pre-condition: Assume all elements have at least one character!
+    // For example: ["app", "banana", "audio", "bear"] -> 2
+    public static int firstLetterA(ArrayList<String> arr)
+    {
+
+    }
+
+    
+    // Algorithm 9: Shift or rotate elements left
+    // For example: [1,2,3,4,5] -> [2,3,4,5,1]
+    public static void shiftLeft(ArrayList<Integer> arr){
+       
+    }
+
+
+    // Algorithm 9.5: Shift or rotate elements right
+    // For example: [1,2,3,4,5] -> [5,1,2,3,4]
+    public static void shiftRight(ArrayList<Integer> arr){
+       
+    }
+
+
+
+    // Algorithm 10: Reverse the order of the elements
+    // For example: ["a", "b", "c", "d"] -> ["d", "c", "b", "a"]
+    public static void reverse(ArrayList<String> arr){
+        
+    }
+}
